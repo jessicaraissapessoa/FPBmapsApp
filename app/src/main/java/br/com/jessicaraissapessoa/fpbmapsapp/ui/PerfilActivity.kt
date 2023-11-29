06a09de9d070ba05cb084cc710c9d1541ea6e6fb
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-11-29 05:13:23 -0300

    Perfil Usuário

---
+++ app/src/main/java/br/com/jessicaraissapessoa/fpbmapsapp/ui/PerfilActivity.kt
@@ -0,0 +1,39 @@
+package br.com.jessicaraissapessoa.fpbmapsapp.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.jessicaraissapessoa.fpbmapsapp.databinding.ActivityPerfilBinding
+
+class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityPerfilBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+
+        binding.btnUlifeAluno.setOnClickListener {
+            acessarUlife("https://aluno.fpb.edu.br/SOL/aluno/index.php/index/seguranca/dev/instituicao/136")
+        }
+
+        binding.btnSalasVirtuais.setOnClickListener {
+            acessarUlife("https://www.ulife.com.br/Login.aspx?ReturnUrl=%2fStudentHome#pageIndex=1")
+        }
+
+        binding.btnLogout.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun acessarUlife(url : String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+}
