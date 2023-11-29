f3f1aa17fbb835b91c1982cc7db831ce22fdc362
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-11-29 03:28:51 -0300

    TelaInicialActivity.kt v1

--- app/src/main/java/br/com/jessicaraissapessoa/fpbmapsapp/ui/TelaInicialActivity.kt
+++ app/src/main/java/br/com/jessicaraissapessoa/fpbmapsapp/ui/TelaInicialActivity.kt
@@ -1,10 +1,8 @@
 package br.com.jessicaraissapessoa.fpbmapsapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import br.com.jessicaraissapessoa.fpbmapsapp.R
 import br.com.jessicaraissapessoa.fpbmapsapp.databinding.ActivityTelaInicialBinding
 
 class TelaInicialActivity : AppCompatActivity() {
@@ -15,8 +13,13 @@ class TelaInicialActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        setupWithNavController(binding.bottomNavigation, navController)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnPerfil.setOnClickListener {
+            startActivity(Intent(this, PerfilActivity::class.java))
+        }
     }
 
 }
